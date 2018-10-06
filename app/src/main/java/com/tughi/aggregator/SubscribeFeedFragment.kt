7e6539df0ec86ccde4ce431a5ff3bb0171416318
Tughi <one@tughi.com>
2018-10-06 22:54:29 +0200

    Add subscribe layout

--- app/src/main/java/com/tughi/aggregator/SubscribeFeedFragment.kt
+++ app/src/main/java/com/tughi/aggregator/SubscribeFeedFragment.kt
@@ -1,5 +1,8 @@
 package com.tughi.aggregator
 
+import android.os.Bundle
+import android.view.*
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class SubscribeFeedFragment : Fragment() {
@@ -7,6 +10,39 @@ class SubscribeFeedFragment : Fragment() {
     companion object {
         const val ARG_TITLE = "title"
         const val ARG_URL = "url"
+    }
+
+    private lateinit var urlTextView: TextView
+    private lateinit var titleTextView: TextView
+    private lateinit var updateModeTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val fragmentView = inflater.inflate(R.layout.subscribe_feed_fragment, container, false)
+        val arguments = arguments!!
+
+        urlTextView = fragmentView.findViewById(R.id.url)
+        urlTextView.text = arguments.getString(ARG_URL)
+
+        titleTextView = fragmentView.findViewById(R.id.title)
+        titleTextView.text = arguments.getString(ARG_TITLE)
+
+        updateModeTextView = fragmentView.findViewById(R.id.update_mode)
+        updateModeTextView.keyListener = null
+        updateModeTextView.setText(R.string.update_mode__default)
+
+        return fragmentView
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater?.inflate(R.menu.subscribe_feed_fragment, menu)
     }
 
     override fun onResume() {
