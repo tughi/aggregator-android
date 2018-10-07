837ea9e5706dd515b02e8857e4f1663e878f29c6
Tughi <one@tughi.com>
2018-10-07 08:07:14 +0200

    Remove test feeds

--- app/src/main/java/com/tughi/aggregator/data/AppDatabase.kt
+++ app/src/main/java/com/tughi/aggregator/data/AppDatabase.kt
@@ -1,12 +1,9 @@
 package com.tughi.aggregator.data
 
-import android.content.ContentValues
 import android.content.Context
-import android.database.sqlite.SQLiteDatabase
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.tughi.aggregator.utilities.DATABASE_NAME
 
 @Database(
@@ -32,30 +29,6 @@ abstract class AppDatabase : RoomDatabase() {
 
         private fun create(context: Context): AppDatabase {
             return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
-                    .addCallback(object : RoomDatabase.Callback() {
-                        override fun onCreate(database: SupportSQLiteDatabase) {
-                            arrayOf(
-                                    ContentValues().apply {
-                                        put("url", "url1")
-                                        put("title", "Aggregator News")
-                                    },
-                                    ContentValues().apply {
-                                        put("url", "url2")
-                                        put("title", "MarsTechnico")
-                                    },
-                                    ContentValues().apply {
-                                        put("url", "url3")
-                                        put("title", "Slashdok")
-                                    },
-                                    ContentValues().apply {
-                                        put("url", "url4")
-                                        put("title", "The Virge")
-                                    }
-                            ).forEach { values ->
-                                database.insert("feeds", SQLiteDatabase.CONFLICT_REPLACE, values)
-                            }
-                        }
-                    })
                     .build()
         }
 
