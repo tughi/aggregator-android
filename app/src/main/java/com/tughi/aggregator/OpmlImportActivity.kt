bfb96f13f2ef4877882336bdfd74df0eab7b4794
Tughi <one@tughi.com>
2018-12-09 20:10:36 +0100

    Subscribe to feeds loaded from OPML files

---
+++ app/src/main/java/com/tughi/aggregator/OpmlImportActivity.kt
@@ -0,0 +1,204 @@
+package com.tughi.aggregator
+
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.tughi.aggregator.data.Feed
+import com.tughi.aggregator.data.UpdateMode
+import com.tughi.aggregator.feeds.OpmlParser
+import com.tughi.aggregator.services.AutoUpdateScheduler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+data class OpmlFeed(val feed: Feed, val excluded: Boolean)
+
+internal class OpmlImportViewModel : ViewModel() {
+
+    val feeds = MutableLiveData<List<OpmlFeed>>()
+
+    fun parseOpml(uri: Uri) {
+        GlobalScope.launch(Dispatchers.IO) {
+            App.instance.contentResolver?.openInputStream(uri)?.use {
+                val feeds = mutableListOf<OpmlFeed>()
+
+                try {
+                    OpmlParser.parse(it, object : OpmlParser.Listener {
+                        override fun onFeedParsed(url: String, title: String, link: String?, customTitle: String?, category: String?, updateMode: UpdateMode) {
+                            feeds.add(OpmlFeed(
+                                    feed = Feed(
+                                            url = url,
+                                            title = title,
+                                            link = link,
+                                            customTitle = customTitle,
+                                            updateMode = updateMode
+                                    ),
+                                    excluded = false
+                            ))
+                        }
+                    })
+                } catch (exception: IOException) {
+                    Log.e(javaClass.name, "Failed to load OPML file", exception)
+                }
+
+                launch(Dispatchers.Main) {
+                    this@OpmlImportViewModel.feeds.value = feeds
+                }
+            }
+        }
+    }
+
+    fun toggleFeed(feed: OpmlFeed) {
+        val newFeeds = mutableListOf<OpmlFeed>()
+
+        feeds.value?.forEach {
+            if (it === feed) {
+                newFeeds.add(it.copy(feed = it.feed, excluded = !it.excluded))
+            } else {
+                newFeeds.add(it)
+            }
+        }
+
+        feeds.value = newFeeds
+    }
+
+}
+
+class OpmlImportActivity : AppActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.opml_import_activity)
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.action_back)
+        }
+
+        val viewModel = ViewModelProviders.of(this).get(OpmlImportViewModel::class.java)
+
+        val feedsAdapter = OpmlFeedsAdapter(viewModel)
+
+        findViewById<RecyclerView>(R.id.feeds).adapter = feedsAdapter
+
+        val subscribeButton = findViewById<Button>(R.id.subscribe)
+
+        subscribeButton.setOnClickListener { _ ->
+            viewModel.feeds.value?.let { feeds ->
+                GlobalScope.launch(Dispatchers.IO) {
+                    val feedDao = AppDatabase.instance.feedDao()
+
+                    feeds.forEach {
+                        if (!it.excluded) {
+                            val feedId = feedDao.insertFeed(it.feed)
+
+                            if (feedId > 0) {
+                                feedDao.updateFeed(feedId, AutoUpdateScheduler.calculateNextUpdateTime(feedId, it.feed.updateMode, 0))
+                            }
+                        }
+                    }
+
+                    AutoUpdateScheduler.schedule()
+                }
+
+                finish()
+            }
+        }
+
+        viewModel.feeds.observe(this, Observer { feeds ->
+            feedsAdapter.submitList(feeds)
+
+            val feedCount = feeds.size
+
+            var excludedFeedCount = 0
+            feeds.forEach { feed ->
+                if (feed.excluded) {
+                    excludedFeedCount++
+                }
+            }
+
+            subscribeButton.isEnabled = feedCount != 0 && feedCount != excludedFeedCount
+        })
+
+        if (savedInstanceState == null) {
+            intent.data?.let {
+                viewModel.parseOpml(it)
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> finish()
+            else -> return super.onOptionsItemSelected(item)
+        }
+
+        return true
+    }
+
+}
+
+internal class OpmlFeedViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    val checkbox: ImageView = itemView.findViewById(R.id.checkbox)
+    val title: TextView = itemView.findViewById(R.id.title)
+    val url: TextView = itemView.findViewById(R.id.url)
+
+    lateinit var feed: OpmlFeed
+
+    fun onBind(feed: OpmlFeed) {
+        this.feed = feed
+
+        if (feed.excluded) {
+            checkbox.setImageResource(R.drawable.check_box_unchecked)
+        } else {
+            checkbox.setImageResource(R.drawable.check_box_checked)
+        }
+        title.text = feed.feed.title
+        url.text = feed.feed.url
+    }
+
+}
+
+internal class OpmlFeedsAdapter(val viewModel: OpmlImportViewModel) : ListAdapter<OpmlFeed, OpmlFeedViewHolder>(FeedDiffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OpmlFeedViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.opml_import_list_item, parent, false)
+        val viewHolder = OpmlFeedViewHolder(itemView)
+
+        itemView.setOnClickListener {
+            viewModel.toggleFeed(viewHolder.feed)
+        }
+
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: OpmlFeedViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+}
+
+object FeedDiffUtil : DiffUtil.ItemCallback<OpmlFeed>() {
+
+    override fun areItemsTheSame(oldItem: OpmlFeed, newItem: OpmlFeed): Boolean = oldItem.feed === newItem.feed
+
+    override fun areContentsTheSame(oldItem: OpmlFeed, newItem: OpmlFeed): Boolean = oldItem.excluded == newItem.excluded
+
+}
---
+++ app/src/main/java/com/tughi/aggregator/feeds/OpmlParser.kt
@@ -0,0 +1,95 @@
+package com.tughi.aggregator.feeds
+
+import android.util.Xml
+import com.tughi.aggregator.data.DefaultUpdateMode
+import com.tughi.aggregator.data.UpdateMode
+import com.tughi.xml.Document
+import com.tughi.xml.Element
+import com.tughi.xml.TagElement
+import org.xml.sax.Attributes
+import org.xml.sax.SAXException
+import java.io.IOException
+import java.io.InputStream
+import java.util.*
+
+object OpmlParser {
+
+    fun parse(inputStream: InputStream, listener: Listener) {
+        val document = Document()
+        val opmlElement = document.addChild(TagElement("opml"))
+        val bodyElement = opmlElement.addChild(TagElement("body"))
+
+        bodyElement.addChild(object : TagElement("outline") {
+            private val path = LinkedList<String>()
+
+            override fun getChild(uri: String, name: String): Element? = if ("outline" == name) this else null
+
+            override fun start(uri: String?, name: String?, attributes: Attributes?) {
+                val text = attributes!!.getValue("text")
+
+                var type: String? = attributes.getValue("type")
+                if (type == null && attributes.getValue("xmlUrl") != null) {
+                    type = "rss"
+                }
+
+                if (type == "rss") {
+                    val feedLink = attributes.getValue("htmlUrl")
+                    var feedCustomTitle = text
+                    var feedTitle = attributes.getValue("title")
+                    if (feedTitle == null) {
+                        if (feedCustomTitle == null) {
+                            feedTitle = "Feed"
+                            feedCustomTitle = feedTitle
+                        } else {
+                            feedTitle = text
+                        }
+                    } else if (feedCustomTitle == null) {
+                        feedCustomTitle = feedTitle
+                    }
+                    val feedUrl = attributes.getValue("xmlUrl")
+                    val feedUpdateMode = attributes.getValue("updateMode").let {
+                        if (it == null) {
+                            return@let DefaultUpdateMode
+                        }
+
+                        return@let UpdateMode.deserialize(it)
+                    }
+                    val feedCategory = if (path.isEmpty()) null else path[0]
+
+                    listener.onFeedParsed(
+                            url = feedUrl,
+                            title = feedTitle,
+                            link = feedLink,
+                            category = feedCategory,
+                            customTitle = feedCustomTitle,
+                            updateMode = feedUpdateMode
+                    )
+                }
+
+                path.add(text)
+            }
+
+            override fun end(namespace: String?, name: String?) {
+                path.removeLast()
+            }
+        })
+
+        try {
+            Xml.parse(inputStream, Xml.Encoding.UTF_8, document.contentHandler)
+        } catch (exception: SAXException) {
+            throw IOException("OPML parsing failed", exception)
+        }
+    }
+
+    interface Listener {
+        fun onFeedParsed(
+                url: String,
+                title: String,
+                link: String?,
+                customTitle: String?,
+                category: String?,
+                updateMode: UpdateMode
+        )
+    }
+
+}
