87ea01f388e6725b55e5c4ba3a59674dec85dbeb
Tughi <one@tughi.com>
2019-02-16 06:40:53 +0100

    Process database entries in a coroutine

--- app/src/main/java/com/tughi/aggregator/activities/main/EntriesFragmentViewModel.kt
+++ app/src/main/java/com/tughi/aggregator/activities/main/EntriesFragmentViewModel.kt
@@ -1,82 +1,78 @@
 package com.tughi.aggregator.activities.main
 
-import android.os.Handler
-import android.os.HandlerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.tughi.aggregator.AppDatabase
 import com.tughi.aggregator.data.EntriesQuery
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 class EntriesFragmentViewModel(entriesQuery: EntriesQuery) : ViewModel() {
 
-    companion object {
-        private const val MSG_PROCESS_DATABASE_ENTRIES = 1
-    }
+    private val databaseEntries = AppDatabase.instance.mainDao().getEntriesFragmentEntries(entriesQuery)
 
-    private val databaseEntries: LiveData<Array<EntriesFragmentEntry>> = AppDatabase.instance.mainDao().getEntriesFragmentEntries(entriesQuery)
-    private val processedDatabaseEntries = MutableLiveData<Array<EntriesFragmentEntry>>()
+    val entries: LiveData<List<EntriesFragmentEntry>> = MediatorLiveData<List<EntriesFragmentEntry>>().also {
+        var currentJob: Job? = null
 
-    private val handlerThread = HandlerThread(javaClass.simpleName).also { it.start() }
-    private val handler = Handler(handlerThread.looper, Handler.Callback {
-        when (it.what) {
-            MSG_PROCESS_DATABASE_ENTRIES -> {
-                @Suppress("UNCHECKED_CAST")
-                processDatabaseEntries(it.obj as Array<EntriesFragmentEntry>)
+        it.addSource(databaseEntries) { databaseEntries ->
+            currentJob?.run {
+                cancel()
             }
-        }
-        return@Callback true
-    })
 
-    val entries: LiveData<List<EntriesFragmentEntry>> = MediatorLiveData<List<EntriesFragmentEntry>>().apply {
-        addSource(databaseEntries) { databaseEntries ->
-            if (databaseEntries.isEmpty()) {
-                value = databaseEntries.asList()
-            } else {
-                handler.removeMessages(MSG_PROCESS_DATABASE_ENTRIES)
-                handler.sendMessage(handler.obtainMessage(MSG_PROCESS_DATABASE_ENTRIES, databaseEntries))
-            }
-        }
-        addSource(processedDatabaseEntries) { entries ->
-            value = entries.asList()
-        }
-    }
-
-    private fun processDatabaseEntries(databaseEntries: Array<EntriesFragmentEntry>) {
-        val oldEntries = processedDatabaseEntries.value
-        if (oldEntries != null && oldEntries.size == databaseEntries.size * 2) {
-            var changed = false
-            for (index in 0 until databaseEntries.size) {
-                if (oldEntries[index * 2 + 1] != databaseEntries[index]) {
-                    changed = true
-                    break
+            currentJob = GlobalScope.launch {
+                if (!isActive) {
+                    return@launch
                 }
-            }
-            if (!changed) {
-                return
-            }
-        }
 
-
-        val newEntries = Array(databaseEntries.size * 2) { index ->
-            when {
-                index == 0 -> databaseEntries[0].copy(readTime = 0, type = EntriesFragmentEntryType.HEADER)
-                index % 2 == 0 -> databaseEntries[index / 2].let {
-                    it.copy(readTime = 0, type = if (it.formattedDate != databaseEntries[index / 2 - 1].formattedDate) EntriesFragmentEntryType.HEADER else EntriesFragmentEntryType.DIVIDER)
+                val oldEntries = it.value
+                if (oldEntries != null && oldEntries.size == databaseEntries.size * 2) {
+                    var changed = false
+                    for (index in 0 until databaseEntries.size) {
+                        if (oldEntries[index * 2 + 1] != databaseEntries[index]) {
+                            changed = true
+                            break
+                        }
+                    }
+                    if (!changed) {
+                        return@launch
+                    }
                 }
-                else -> databaseEntries[index / 2]
+
+                if (!isActive) {
+                    return@launch
+                }
+
+                val newEntries = Array(databaseEntries.size * 2) { index ->
+                    when {
+                        index == 0 -> databaseEntries[0].let { entry ->
+                            entry.copy(
+                                    id = -entry.id,
+                                    readTime = 0,
+                                    type = EntriesFragmentEntryType.HEADER
+                            )
+                        }
+                        index % 2 == 0 -> databaseEntries[index / 2].let { entry ->
+                            entry.copy(
+                                    id = -entry.id,
+                                    readTime = 0,
+                                    type = if (entry.formattedDate != databaseEntries[index / 2 - 1].formattedDate) EntriesFragmentEntryType.HEADER else EntriesFragmentEntryType.DIVIDER
+                            )
+                        }
+                        else -> databaseEntries[index / 2]
+                    }
+                }
+
+                if (!isActive) {
+                    return@launch
+                }
+
+                it.postValue(newEntries.asList())
             }
         }
-
-        processedDatabaseEntries.postValue(newEntries)
-    }
-
-    override fun onCleared() {
-        handlerThread.quit()
-
-        super.onCleared()
     }
 
     class Factory(private val entriesQuery: EntriesQuery) : ViewModelProvider.Factory {
