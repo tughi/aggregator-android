281641e7bc0df111a3e62ce2d73cd319994ac7ca
Tughi <one@tughi.com>
2019-03-20 20:26:33 +0100

    Add support for querying tag.entry_count and tag.unread_entry_count

--- app/src/main/java/com/tughi/aggregator/data/Tags.kt
+++ app/src/main/java/com/tughi/aggregator/data/Tags.kt
@@ -16,6 +16,8 @@ object Tags : Repository<Tags.Column, Tags.TableColumn, Tags.UpdateCriteria, Tag
     object ID : Column("id", "t.id"), TableColumn
     object NAME : Column("name", "t.name"), TableColumn
     object EDITABLE : Column("editable", "t.editable"), TableColumn
+    object ENTRY_COUNT : Column("entry_count", "COUNT(1)", arrayOf("entry_tag", "tag"))
+    object UNREAD_ENTRY_COUNT : Column("unread_entry_count", "SUM(CASE WHEN e.read_time = 0 THEN 1 WHEN e.pinned_time THEN 1 ELSE 0 END)", arrayOf("entry", "entry_tag", "tag"))
 
     fun addTag(entryId: Long, tagId: Long) = Database.replace("entry_tag", contentValuesOf("entry_id" to entryId, "tag_id" to tagId, "tag_time" to System.currentTimeMillis()))
 
@@ -29,11 +31,52 @@ object Tags : Repository<Tags.Column, Tags.TableColumn, Tags.UpdateCriteria, Tag
         fun config(builder: SupportSQLiteQueryBuilder, columns: Array<out Column>)
     }
 
+    class AllTagsQueryCriteria : QueryCriteria {
+        override fun config(builder: SupportSQLiteQueryBuilder, columns: Array<out Column>) {
+        }
+    }
+
     abstract class QueryHelper<Row>(vararg columns: Column) : Repository.QueryHelper<Column, QueryCriteria, Row>(columns) {
 
+        private val tables: String
+
+        init {
+            val tables = StringBuilder("tag t")
+
+            var entry = false
+            var entryTag = false
+            for (column in columns) {
+                if (column !is TableColumn) {
+                    when (column) {
+                        is ENTRY_COUNT -> {
+                            entryTag = true
+                        }
+                        is UNREAD_ENTRY_COUNT -> {
+                            entry = true
+                            entryTag = true
+                        }
+                    }
+                }
+                if (entry && entryTag) {
+                    break
+                }
+            }
+            if (entryTag) {
+                tables.append(" LEFT JOIN entry_tag et ON et.tag_id = t.id")
+            }
+            if (entry) {
+                tables.append(" LEFT JOIN entry e ON e.id = et.entry_id")
+            }
+
+            this.tables = tables.toString()
+        }
+
+
         override fun createQuery(criteria: QueryCriteria): SupportSQLiteQuery = SupportSQLiteQueryBuilder
-                .builder("tag t")
+                .builder(tables)
                 .columns(Array(columns.size) { "${columns[it].projection} AS ${columns[it].name}" })
+                .groupBy("t.id")
+                .orderBy("t.name")
                 .also { criteria.config(it, columns) }
                 .create()
 
