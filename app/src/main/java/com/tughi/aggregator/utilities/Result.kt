07899c0d4b147dd1377dd3b0f873dd9add2194c8
Tughi <one@tughi.com>
2019-06-07 13:47:59 +0300

    Add helpers to support Railway Oriented Programming

--- app/src/main/java/com/tughi/aggregator/utilities/Result.kt
+++ app/src/main/java/com/tughi/aggregator/utilities/Result.kt
@@ -1,6 +1,15 @@
 package com.tughi.aggregator.utilities
 
-sealed class Result<out T : Any>
-data class Success<out T : Any>(val data: T) : Result<T>()
-data class Failure(val error: Throwable?) : Result<Nothing>()
+sealed class Result<out T>
+data class Success<T>(val data: T) : Result<T>()
+data class Failure(val error: Throwable) : Result<Nothing>()
 
+fun <I, O> Result<I>.then(transform: (I) -> Result<O>) = when (this) {
+    is Success -> transform(data)
+    is Failure -> Failure(error)
+}
+
+fun <T> Result<T>.otherwise(catch: (Throwable) -> Unit) = when (this) {
+    is Success -> Unit
+    is Failure -> catch(error)
+}
