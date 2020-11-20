c340e83973f6448e6435cb88e03a83cf73a2a1ff
Tughi <one@tughi.com>
2020-11-20 11:56:59 +0100

    Add subscribe button to the bottom bar

--- app/src/main/java/com/tughi/aggregator/activities/subscribe/SubscribeFeedFragment.kt
+++ app/src/main/java/com/tughi/aggregator/activities/subscribe/SubscribeFeedFragment.kt
@@ -4,19 +4,21 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.tughi.aggregator.R
+import com.tughi.aggregator.activities.cleanupmode.CleanupModeActivity
+import com.tughi.aggregator.activities.cleanupmode.startCleanupModeActivity
+import com.tughi.aggregator.activities.cleanupmode.toString
 import com.tughi.aggregator.activities.updatemode.UpdateModeActivity
 import com.tughi.aggregator.activities.updatemode.startUpdateModeActivity
 import com.tughi.aggregator.activities.updatemode.toString
+import com.tughi.aggregator.data.CleanupMode
+import com.tughi.aggregator.data.DefaultCleanupMode
 import com.tughi.aggregator.data.DefaultUpdateMode
 import com.tughi.aggregator.data.Feeds
 import com.tughi.aggregator.data.UpdateMode
@@ -36,19 +38,15 @@ class SubscribeFeedFragment : Fragment() {
         const val ARG_LINK = "link"
 
         private const val REQUEST_UPDATE_MODE = 1
+        private const val REQUEST_CLEANUP_MODE = 3
     }
 
     private lateinit var viewModel: SubscribeFeedFragmentViewModel
 
     private lateinit var urlTextView: TextView
     private lateinit var titleTextView: TextView
-    private lateinit var updateModeTextView: DropDownButton
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setHasOptionsMenu(true)
-    }
+    private lateinit var updateModeView: DropDownButton
+    private lateinit var cleanupModeView: DropDownButton
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val fragmentView = inflater.inflate(R.layout.subscribe_feed_fragment, container, false)
@@ -63,23 +61,57 @@ class SubscribeFeedFragment : Fragment() {
         titleTextView = fragmentView.findViewById(R.id.title)
         titleTextView.text = arguments.getString(ARG_TITLE)
 
-        updateModeTextView = fragmentView.findViewById(R.id.update_mode)
-        updateModeTextView.setOnClickListener {
+        updateModeView = fragmentView.findViewById(R.id.update_mode)
+        updateModeView.setOnClickListener {
             val currentUpdateMode = viewModel.updateMode.value ?: return@setOnClickListener
             startUpdateModeActivity(REQUEST_UPDATE_MODE, currentUpdateMode)
         }
 
-        viewModel.updateMode.observe(viewLifecycleOwner, Observer {
-            updateModeTextView.setText(it.toString(updateModeTextView.context))
+        viewModel.updateMode.observe(viewLifecycleOwner, {
+            updateModeView.setText(it.toString(updateModeView.context))
         })
 
+        cleanupModeView = fragmentView.findViewById(R.id.cleanup_mode)
+        cleanupModeView.setOnClickListener {
+            val currentCleanupMode = viewModel.cleanupMode.value ?: return@setOnClickListener
+            startCleanupModeActivity(REQUEST_CLEANUP_MODE, currentCleanupMode)
+        }
+
+        viewModel.cleanupMode.observe(viewLifecycleOwner, {
+            cleanupModeView.setText(it.toString(cleanupModeView.context))
+        })
+
+        val subscribeButton = fragmentView.findViewById<Button>(R.id.subscribe)
+        subscribeButton.setOnClickListener {
+            val title = arguments.getString(ARG_TITLE)!!
+            val customTitle = titleTextView.text.toString()
+            val link = arguments.getString(ARG_LINK)
+
+            GlobalScope.launch(Dispatchers.IO) {
+                val feedId = Feeds.insert(
+                        Feeds.URL to urlTextView.text.toString(),
+                        Feeds.TITLE to title,
+                        Feeds.CUSTOM_TITLE to if (customTitle != title) customTitle else null,
+                        Feeds.LINK to link,
+                        Feeds.UPDATE_MODE to (viewModel.updateMode.value ?: DefaultUpdateMode).serialize(),
+                        Feeds.CLEANUP_MODE to (viewModel.cleanupMode.value ?: DefaultCleanupMode).serialize()
+                )
+
+                launch {
+                    AutoUpdateScheduler.scheduleFeed(feedId)
+
+                    backupFeeds()
+                }
+
+                launch(Dispatchers.Main) {
+                    FaviconUpdateScheduler.schedule()
+
+                    activity?.finish()
+                }
+            }
+        }
+
         return fragmentView
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-
-        inflater.inflate(R.menu.subscribe_feed_fragment, menu)
     }
 
     override fun onResume() {
@@ -88,44 +120,18 @@ class SubscribeFeedFragment : Fragment() {
         activity?.setTitle(R.string.title_add_feed)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.add -> {
-                val arguments = requireArguments()
-                val title = arguments.getString(ARG_TITLE)!!
-                val customTitle = titleTextView.text.toString()
-                val link = arguments.getString(ARG_LINK)
-
-                GlobalScope.launch(Dispatchers.IO) {
-                    val feedId = Feeds.insert(
-                            Feeds.URL to urlTextView.text.toString(),
-                            Feeds.TITLE to title,
-                            Feeds.CUSTOM_TITLE to if (customTitle != title) customTitle else null,
-                            Feeds.LINK to link,
-                            Feeds.UPDATE_MODE to (viewModel.updateMode.value ?: DefaultUpdateMode).serialize()
-                    )
-
-                    launch {
-                        AutoUpdateScheduler.scheduleFeed(feedId)
-
-                        backupFeeds()
-                    }
-
-                    launch(Dispatchers.Main) {
-                        FaviconUpdateScheduler.schedule()
-
-                        activity?.finish()
-                    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_UPDATE_MODE -> {
+                    val serializedUpdateMode = data?.getStringExtra(UpdateModeActivity.EXTRA_UPDATE_MODE) ?: return
+                    viewModel.updateMode.value = UpdateMode.deserialize(serializedUpdateMode)
+                }
+                REQUEST_CLEANUP_MODE -> {
+                    val serializedCleanupMode = data?.getStringExtra(CleanupModeActivity.EXTRA_CLEANUP_MODE) ?: return
+                    viewModel.cleanupMode.value = CleanupMode.deserialize(serializedCleanupMode)
                 }
             }
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_UPDATE_MODE && resultCode == Activity.RESULT_OK) {
-            val serializedUpdateMode = data?.getStringExtra(UpdateModeActivity.EXTRA_UPDATE_MODE) ?: return
-            viewModel.updateMode.value = UpdateMode.deserialize(serializedUpdateMode)
         }
     }
 
--- app/src/main/java/com/tughi/aggregator/activities/subscribe/SubscribeFeedFragmentViewModel.kt
+++ app/src/main/java/com/tughi/aggregator/activities/subscribe/SubscribeFeedFragmentViewModel.kt
@@ -3,11 +3,14 @@ package com.tughi.aggregator.activities.subscribe
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.tughi.aggregator.data.CleanupMode
+import com.tughi.aggregator.data.DefaultCleanupMode
 import com.tughi.aggregator.data.DefaultUpdateMode
 import com.tughi.aggregator.data.UpdateMode
 
 class SubscribeFeedFragmentViewModel : ViewModel() {
 
+    var cleanupMode = MutableLiveData<CleanupMode>().apply { value = DefaultCleanupMode }
     var updateMode = MutableLiveData<UpdateMode>().apply { value = DefaultUpdateMode }
 
     class Feed(val url: String, val title: String, val link: String?, val updateMode: UpdateMode, val customTitle: String?)
