b6fd87ec75f14735e49939ac80f40d2fa467f9b2
Tughi <one@tughi.com>
2023-03-20 21:45:13 +0100

    Check POST_NOTIFICATIONS permission

--- app/src/main/java/com/tughi/aggregator/Notifications.kt
+++ app/src/main/java/com/tughi/aggregator/Notifications.kt
@@ -1,12 +1,15 @@
 package com.tughi.aggregator
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.os.Build
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.tughi.aggregator.activities.notifications.NewEntriesActivity
@@ -61,7 +64,9 @@ object Notifications {
                             .setAutoCancel(true)
                             .build()
 
-                        notificationManager.notify(NOTIFICATION__NEW_ENTRIES__MY_FEED, notification)
+                        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                            notificationManager.notify(NOTIFICATION__NEW_ENTRIES__MY_FEED, notification)
+                        }
                     } else {
                         notificationManager.cancel(NOTIFICATION__NEW_ENTRIES__MY_FEED)
                     }
